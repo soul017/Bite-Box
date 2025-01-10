20582342ca4ea2171ea8b097885795c15747d42a
soul017 <sengar017lokesh@gmail.com>
2025-01-10 22:27:31 +0530

    Updates

---
+++ Grocery.3c06841b.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire94c2;(0,e.register)("loR3J",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>t,set:void 0,enumerable:!0,configurable:!0});var i=e("ayMG0"),n=e("kjGqO"),t=()=>!1===(0,n.default)()?/*#__PURE__*/(0,i.jsx)("h1",{children:"Looks Like You Are Offline, Check Your Internet Connection"}):/*#__PURE__*/(0,i.jsx)("h1",{children:"Another big app having separate build"})});
+//# sourceMappingURL=Grocery.3c06841b.js.map
